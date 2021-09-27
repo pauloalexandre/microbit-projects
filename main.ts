4f2385378994ffbe24f3d2a502d3b9ddb1a11140
Paulo Dias <pauloalexandr@gmail.com>
2021-09-27 13:03:51 +0100

    Atualização pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,27 @@
-
+input.onButtonPressed(Button.A, function () {
+    número += -1
+})
+input.onGesture(Gesture.Shake, function () {
+    número = inicial
+})
+input.onButtonPressed(Button.AB, function () {
+    if (número == segredo) {
+        basic.showIcon(IconNames.Yes)
+    } else if (número < segredo) {
+        basic.showArrow(ArrowNames.North)
+    } else {
+        basic.showArrow(ArrowNames.South)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    número += 1
+})
+let segredo = 0
+let número = 0
+let inicial = 0
+inicial = 5
+número = inicial
+segredo = randint(0, 9)
+basic.forever(function () {
+    basic.showNumber(número)
+})
